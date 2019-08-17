8087ceccf25f40070a3b5585f9926b4ea0119128
Максим Ильин <negamaxi@gmail.com>
2019-08-17 10:28:19 +0300

    fix: throw custom error when no handler prop provided

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,5 @@
 ## [1.0.1](https://github.com/draftup/react-event-boundary/compare/v1.0.0...v1.0.1) (2019-08-16)
 
-
 ### Bug Fixes
 
-* update installation script in README.md ([6a53f55](https://github.com/draftup/react-event-boundary/commit/6a53f55))
+- update installation script in README.md ([6a53f55](https://github.com/draftup/react-event-boundary/commit/6a53f55))
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,32 @@
 import React from "react";
 
-type EventBoundaryProps<Event> = React.FC<{
+// Types
+
+type EventBoundary<Event> = React.FC<{
   handler: React.Dispatch<Event>;
 }>;
 
-type EventBoundaryCreator = <Event>(
-  DispatchContext: React.Context<React.Dispatch<Event>>
-) => React.FC<EventBoundaryProps<Event>>;
+type DispatchContext<Event> = React.Context<React.Dispatch<Event>>;
 
-export const createEventBoundary: EventBoundaryCreator = DispatchContext => {
-  const EventBoundary = props => {
+export type EventBoundaryCreator = <Event>(
+  DispatchContext: DispatchContext<Event>
+) => EventBoundary<Event>;
+
+// Utils
+
+class EventBoundaryError extends Error {}
+
+// Lib
+
+export function createEventBoundary<Event>(
+  DispatchContext: DispatchContext<Event>
+) {
+  const EventBoundary: EventBoundary<Event> = props => {
     const { children, handler: localHandler } = props;
+
+    if (!localHandler) {
+      throw new EventBoundaryError('Missing required "handler" prop.');
+    }
 
     const upperDispatch = React.useContext(DispatchContext);
 
@@ -30,4 +46,4 @@ export const createEventBoundary: EventBoundaryCreator = DispatchContext => {
   };
 
   return EventBoundary;
-};
+}
